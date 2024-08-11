b4031c17da474b6eb0ad77e496dd783639be1f81
Vladislav Churilov <Nawaharjan@gmail.com>
2024-08-11 23:45:42 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkLvl2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фразу которая будет повторятся");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Сколько раз необходимо повторить фразу?");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(userInput);
+            }
+        }
+    }
+}
